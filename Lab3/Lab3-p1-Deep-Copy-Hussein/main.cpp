340a08d1544c332cd72d03921289e46160cc4e47
Hussein Elmlah <147069168+hussein-elmlah@users.noreply.github.com>
2023-11-09 15:38:11 +0200

    Add files via upload

---
+++ Lab3/Lab3-p1-Deep-Copy-Hussein/main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+using namespace std;
+
+class Stack
+{
+private:
+    int* arr;
+    int arrSize;
+    int top;
+public:
+    /*
+    Stack(Stack &s)
+    {
+        arrSize = s.arrSize;
+        top = s.top;
+        arr = new int[arrSize]{0};
+        for(int i=0; i<=top; i++)
+        {
+            arr[i] = s.arr[i];
+        }
+    }
+    */
+    Stack(int arraySize)
+    {
+        cout<<"constructor"<<endl;
+        arrSize = arraySize;
+        arr = new int[arrSize] {0};
+        top = -1;
+    }
+    int getSize(void)
+    {
+        return top;
+    }
+    void push(int x)
+    {
+        if(top < arrSize-1)
+        {
+            top++;
+            arr[top] = x;
+        }
+        else
+        {
+            cout<<"full"<<endl;
+        }
+    }
+    int pop(void)
+    {
+        if(top > -1)
+        {
+            top--;
+            cout<<arr[top+1]<<endl;
+        }
+        else
+        {
+            cout<<"empty"<<endl;
+        }
+    }
+    void display(void)
+    {
+        for(int i=0; i<=top; i++)
+        {
+            cout<<"element "<<i<<" : "<<arr[i]<<endl;
+        }
+    }
+    ~Stack()
+    {
+        cout<<"destructor"<<endl;
+        delete[] arr;
+    }
+    friend void displayStack(Stack s);
+
+};//Class End
+
+void displayStack(Stack s)
+{
+    for(int i=0; i<=s.top; i++)
+    {
+        cout<<"element "<<i<<" : "<<s.arr[i]<<endl;
+    }
+}
+
+void fun(Stack s)
+{
+    s.pop();
+    s.pop();
+}
+
+int main()
+{
+
+    Stack stack1(5);
+    fun(stack1);
+    stack1.pop();//empty
+    stack1.push(1);
+    stack1.push(5);
+    stack1.push(6);
+    stack1.pop();//6
+    stack1.pop();//5
+    fun(stack1);
+    fun(stack1);
+    stack1.push(7);
+    stack1.pop();//7
+    stack1.pop();//1
+    stack1.pop();//Empty
+    stack1.push(7);
+    stack1.pop();//7
+    //1567Empty7
+
+
+
+    //stack1.display();
+    //displayStack(stack1);
+
+    return 0;
+}
